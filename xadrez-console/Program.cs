548877ed4a2de17b829f3335b4f0d1024b5f2fbb
ifWelker <fabiowelker@outlook.com>
2023-04-10 22:45:53 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -15,18 +15,25 @@ namespace xadrez
                 while (!partida.terminada)
                 {
                     Console.Clear();
-                    Tela.imprimirTabueiro(partida.tab);
+                    Tela.imprimirTabuleiro(partida.tab);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
                     partida.executaMovimento(origem, destino);
                 }
 
-                Tela.imprimirTabueiro(partida.tab);
+                Tela.imprimirTabuleiro(partida.tab);
             }
             catch (TabuleiroException e)
             {
